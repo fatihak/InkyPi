bc58386d43df5edd5f3d7d8cfc0c1fd70783ab09
Jason Schwarzenberger <jason@js.org.nz>
2025-01-11 00:56:11 +1300

    move image manipulation into image_calendar.py.

--- src/plugins/calendar/calendar.py
+++ src/plugins/calendar/calendar.py
@@ -1,31 +1,13 @@
 import logging
-import requests
 import itertools
-import calendar
-import icalendar
-import recurring_ical_events
 import pytz
-from datetime import datetime, timedelta
-from PIL import Image, ImageDraw
+from datetime import datetime
 from utils.app_utils import get_font
 from plugins.base_plugin.base_plugin import BasePlugin
-from plugins.calendar.image_calendar import ImageCalendar
+from plugins.calendar.image_calendar import ImageCalendar, COLOR_PAIRS
 
 logger = logging.getLogger(__name__)
 DEFAULT_TIMEZONE = "US/Eastern"
-
-COLOR_PAIRS = [
-    ("white", "deepskyblue"),
-    ("black", "chartreuse"),
-    ("white", "deeppink"),
-    ("black", "orange"),
-    ("white", "blue"),
-    ("white", "forestgreen"),
-    ("white", "chocolate"),
-    ("white", "red"),
-    ("black", "lime"),
-    ("black", "yellow"),
-]
 
 
 class Calendar(BasePlugin):
@@ -46,102 +28,24 @@ class Calendar(BasePlugin):
             dimensions = dimensions[::-1]
 
         current_time = datetime.now(tz)
-        range_start = current_time.replace(day=1, hour=0, minute=0, second=0)
-        _, last_day = calendar.monthrange(current_time.year, current_time.month)
-        range_end = current_time.replace(day=last_day, hour=23, minute=59, second=59)
-
-        cal = ImageCalendar(current_time.year, current_time.month)
+        cal = ImageCalendar(current_time)
 
         color_cycle = itertools.cycle(COLOR_PAIRS)
         for url in urls:
-            ical = self.fetch_calendar_from_url(url)
-            if ical:
-                events = recurring_ical_events.of(ical).between(range_start, range_end)
-                text_color, background_color = next(color_cycle)
-                for event in events:
-                    start = event["DTSTART"].dt
-                    end = event["DTEND"].dt
-                    summary = event["SUMMARY"]
-                    if isinstance(start, datetime):
-                        time = start.strftime("%H:%M").strip()
-                        cal.add_event(
-                            start.day,
-                            summary,
-                            time=time,
-                            color=text_color,
-                            background=background_color,
-                        )
-                    else:
-                        cal.add_event(
-                            start.day,
-                            summary,
-                            color=text_color,
-                            background=background_color,
-                        )
-                        next_day = max(start, range_start.date()) + timedelta(days=1)
-                        last = (
-                            min(end, range_end).date()
-                            if isinstance(end, datetime)
-                            else min(end, range_end.date())
-                        )
-                        while next_day < last:
-                            cal.add_event(
-                                next_day.day,
-                                summary,
-                                color=text_color,
-                                background=background_color,
-                            )
-                            next_day += timedelta(days=1)
+            cal.load_ical_url(url, next(color_cycle))
 
-        dt = range_start
-        while dt.day < current_time.day:
-            cal.slash_day(dt.day, direction="down" if dt.day % 2 == 0 else "up")
-            dt += timedelta(days=1)
-
+        cal.slash_past_days()
         cal.color_day(current_time.day, "lightblue")
+
         img = cal.render(
             dimensions,
+            title_height=20,
             header_height=18,
             event_height=12,
+            title_font=get_font("napoli", 20),
             header_font=get_font("napoli", 18),
             day_font=get_font("napoli", 12),
             event_font=get_font("jost", 12),
         )
 
-        text = Image.new("RGBA", dimensions, (0, 0, 0, 0))
-        font_size = 20
-        fnt = get_font("napoli", font_size)
-        text_draw = ImageDraw.Draw(text)
-
-        w, h = dimensions
-        time_str = current_time.strftime("%H:%M")
-        month_str = calendar.TextCalendar().formatmonthname(
-            current_time.year, current_time.month, w, withyear=True
-        )
-        text_draw.text(
-            (10, h - font_size),
-            time_str,
-            font=fnt,
-            anchor="lt",
-            fill=(0, 0, 0) + (255,),
-        )
-        text_draw.text(
-            (w - 10, h - font_size),
-            month_str.strip(),
-            font=fnt,
-            anchor="rt",
-            fill=(0, 0, 0) + (255,),
-        )
-
-        combined = Image.alpha_composite(img, text)
-        return combined
-
-    @staticmethod
-    def fetch_calendar_from_url(url):
-        try:
-            response = requests.get(url)
-            response.raise_for_status()
-            return icalendar.Calendar.from_ical(response.text)
-        except requests.exceptions.RequestException as e:
-            logger.error(f"Error fetching calendar from {url}: {e}")
-            return None
+        return img
--- src/plugins/calendar/image_calendar.py
+++ src/plugins/calendar/image_calendar.py
@@ -1,26 +1,107 @@
+import itertools
+import requests
 import calendar
+import icalendar
+import recurring_ical_events
+from datetime import datetime, timedelta
 from PIL import Image, ImageDraw
+
+COLOR_PAIRS = [
+    ("white", "deepskyblue"),
+    ("black", "chartreuse"),
+    ("white", "deeppink"),
+    ("black", "orange"),
+    ("white", "blue"),
+    ("white", "forestgreen"),
+    ("white", "chocolate"),
+    ("white", "red"),
+    ("black", "lime"),
+    ("black", "yellow"),
+]
+
+def fetch_ical(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return icalendar.Calendar.from_ical(response.text)
+    except requests.exceptions.RequestException as e:
+        return None
 
 
 class ImageCalendar:
-    def __init__(self, year, month):
-        self.year = year
-        self.month = month
-        self.calendar = calendar.monthcalendar(year, month)
+    def __init__(self, current_time):
+        self.current_time = current_time
+        self.day = current_time.day
+        self.year = current_time.year
+        self.month = current_time.month
+        self.calendar = calendar.monthcalendar(self.year, self.month)
         self.events = dict()
         self.colors = dict()
         self.slashed = dict()
 
-    def add_event(self, day, summary, time=None, color=None, background=None):
-        self.events[day] = self.events.get(day, []) + [
-            (summary, time, color, background)
-        ]
+        _, last_day = calendar.monthrange(current_time.year, current_time.month)
+        self.range_start = current_time.replace(
+            day=1, hour=0, minute=0, second=0, microsecond=0
+        )
+        self.range_end = current_time.replace(
+            day=last_day, hour=23, minute=59, second=59, microsecond=999999
+        )
 
     def color_day(self, day, color):
         self.colors[day] = color
 
     def slash_day(self, day, slashed=True, direction="down"):
         self.slashed[day] = direction if slashed else None
+
+    def slash_past_days(self):
+        dt = self.range_start
+        while dt.day < self.current_time.day:
+            self.slash_day(dt.day, direction="down" if dt.day % 2 == 0 else "up")
+            dt += timedelta(days=1)
+
+    def add_event(self, day, summary, time=None, color=None, background=None):
+        self.events[day] = self.events.get(day, []) + [
+            (summary, time, color, background)
+        ]
+
+    def load_ical_url(self, url, color):
+        range_start = self.range_start
+        range_end = self.range_end
+        text_color, background_color = color
+        ical = fetch_ical(url)
+        if ical:
+            events = recurring_ical_events.of(ical).between(
+                range_start.date(), range_end.date()
+            )
+            for event in events:
+                start = event["DTSTART"].dt
+                end = event["DTEND"].dt
+                summary = event["SUMMARY"]
+                if isinstance(start, datetime):
+                    time = start.strftime("%H:%M").strip()
+                    self.add_event(
+                        start.day,
+                        summary,
+                        time=time,
+                        color=text_color,
+                        background=background_color,
+                    )
+                else:
+                    next_day = start
+                    last = (
+                        min(end, range_end).date()
+                        if isinstance(end, datetime)
+                        else min(end, range_end.date())
+                    )
+                    while next_day < last:
+                        if next_day >= range_start.date():
+                            self.add_event(
+                                next_day.day,
+                                summary,
+                                color=text_color,
+                                background=background_color,
+                            )
+                        next_day += timedelta(days=1)
 
     def render(
         self,
@@ -30,11 +111,17 @@ class ImageCalendar:
         border_width=1,
         padding=1,
         margin=1,
+        title_height=20,
         header_height=18,
         event_height=12,
+        title_font=None,
         header_font=None,
         day_font=None,
         event_font=None,
+        show_time_line=True,
+        show_month=True,
+        show_year=True,
+        show_time=True,
     ):
 
         width, height = dimensions
@@ -45,18 +132,58 @@ class ImageCalendar:
         columns = len(self.calendar[0])
 
         column_width = (width - margin) / columns
-        row_height = (height - header_height - margin) / rows
+        row_height = (height - title_height - header_height - margin) / rows
+
+        draw.rectangle(
+            [(0, 0), (width - margin, title_height)],
+            fill=None,
+            outline=border_color,
+            width=border_width,
+        )
+
+        month_str = None
+        if show_month:
+            month_str = calendar.TextCalendar().formatmonthname(
+                self.year, self.month, width, withyear=show_year
+            )
+        elif show_year:
+            month_str = str(self.year)
+        if month_str is not None:
+            draw.text(
+                (width - 10, border_width + title_height / 2),
+                month_str.strip(),
+                font=title_font,
+                font_size=title_height,
+                anchor="rm",
+                fill=border_color,
+            )
+
+        if show_time:
+            draw.text(
+                (10, border_width + title_height / 2),
+                self.current_time.strftime("%H:%M"),
+                font=title_font,
+                font_size=title_height,
+                anchor="lm",
+                fill=border_color,
+            )
 
         for day in range(columns):
             x = day * column_width
             draw.rectangle(
-                [(x, 0), (x + column_width, 0 + header_height)],
+                [
+                    (x, title_height),
+                    (x + column_width, 0 + title_height + header_height),
+                ],
                 fill=None,
                 outline=border_color,
                 width=border_width,
             )
             draw.text(
-                (x + column_width / 2, 0 + header_height / 2),
+                (
+                    x + column_width / 2,
+                    0 + border_width + title_height + header_height / 2,
+                ),
                 calendar.day_name[day],
                 anchor="mm",
                 font=header_font,
@@ -66,7 +193,7 @@ class ImageCalendar:
 
             for week in range(rows):
                 x = day * column_width
-                y = week * row_height + header_height
+                y = week * row_height + title_height + header_height
                 date = self.calendar[week][day]
                 fill = self.colors.get(date, background_color)
                 draw.rectangle(
@@ -140,6 +267,18 @@ class ImageCalendar:
                         )
                         line += 1
 
+                    if show_time_line and date == self.day:
+                        delta = self.current_time - self.current_time.replace(
+                            hour=0, minute=0, second=0, microsecond=0
+                        )
+                        time_percent = delta.total_seconds() / 86400
+                        time_y = y + row_height * time_percent
+                        draw.line(
+                            [(x, time_y), (x + column_width, time_y)],
+                            fill=border_color,
+                            width=border_width,
+                        )
+
                     slashed = self.slashed.get(date, None)
                     if slashed == "down":
                         draw.line(
@@ -166,6 +305,7 @@ class ImageCalendar:
                         outline=fill,
                         width=padding,
                     )
+
         return base
 
 
@@ -178,10 +318,19 @@ if __name__ == "__main__":
     parser.add_argument(
         "--output", type=str, default="calendar.png", help="Output file path"
     )
-    parser.add_argument("--width", type=str, default=800, help="Width")
-    parser.add_argument("--height", type=str, default=600, help="Height")
+    parser.add_argument("--width", type=int, default=800, help="Width")
+    parser.add_argument("--height", type=int, default=600, help="Height")
+    parser.add_argument("--url", type=str, action='append', help="iCal URL to load events from")
     args = parser.parse_args()
 
-    cal = ImageCalendar(args.year, args.month)
+    current_time = datetime.now().replace(year=args.year, month=args.month)
+    cal = ImageCalendar(current_time)
+    color_cycle = itertools.cycle(COLOR_PAIRS)
+    for url in args.url or []:
+        cal.load_ical_url(url, next(color_cycle))
+
+    cal.slash_past_days()
+    cal.color_day(cal.current_time.day, "lightblue")
+
     image = cal.render((args.width, args.height))
     image.save(args.output)
