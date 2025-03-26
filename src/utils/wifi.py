d2ced668103c97ce2724aa7504a0fac4b78d269c
IndexOutOfMJ <admin@indexoutofmj.net>
2025-03-26 14:30:29 +0100

    Now with hotspot

--- src/utils/wifi.py
+++ src/utils/wifi.py
@@ -1,13 +1,13 @@
 import os
 
 def open_hotspot():
-    os.system("nmcli device wifi hotspot ifname wlan0 ssid 'INKY' password 'INKY' band bg")
+    os.system("sudo nmcli device wifi hotspot ifname wlan0 ssid 'INKY' password 'SuperSecret123' band bg")
 
 def close_hotspot():
-    os.system("nmcli connection down Hotspot")
+    os.system("sudo nmcli connection down Hotspot")
 
 def connect_to_wifi(ssid, password):
-    os.system(f"nmcli device wifi connect '{ssid}' password '{password}'")
+    os.system(f"sudo nmcli device wifi connect '{ssid}' password '{password}'")
 
 def is_connected():
     ssid = os.popen("iwgetid -r").read().strip()
