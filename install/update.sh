4aa9ba5524d724214977d828128b63fab53beb3a
phattmatt <19799332+phattmatt@users.noreply.github.com>
2025-06-07 21:48:08 +0100

    Modify install.sh and update.sh to upgrade pip, and suppress warnings on pip package installs and upgrades.

--- install/update.sh
+++ install/update.sh
@@ -55,12 +55,12 @@ source "$VENV_PATH/bin/activate"
 
 # Upgrade pip
 echo "Upgrading pip..."
-$VENV_PATH/bin/python -m pip install --upgrade pip setuptools wheel > /dev/null && echo_success "Pip upgraded successfully."
+$VENV_PATH/bin/python -m pip install --upgrade pip setuptools > /dev/null && echo_success "Pip upgraded successfully."
 
 # Install or update Python dependencies
 if [ -f "$PIP_REQUIREMENTS_FILE" ]; then
   echo "Updating Python dependencies..."
-  $VENV_PATH/bin/python -m pip install --upgrade -r "$PIP_REQUIREMENTS_FILE" > /dev/null && echo_success "Dependencies updated successfully."
+  $VENV_PATH/bin/python -m pip install --upgrade -r "$PIP_REQUIREMENTS_FILE" -qq > /dev/null && echo_success "Dependencies updated successfully."
 else
   echo_error "ERROR: Requirements file $PIP_REQUIREMENTS_FILE not found!"
   exit 1
