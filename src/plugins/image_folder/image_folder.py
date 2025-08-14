272500193899a4ef6f5316f7f5434583bdcbc8f7
Ryan Schelong <zapper1942@gmail.com>
2025-08-13 22:45:20 -0400

    fix index out of range issue remove image resizing in favor of doing it externally adding safeguard for images that are too large overwhelming the swap file and crashing the server

--- src/plugins/image_folder/image_folder.py
+++ src/plugins/image_folder/image_folder.py
@@ -10,35 +10,19 @@ logger = logging.getLogger(__name__)
 
 class ImageFolder(BasePlugin):
 
-    def downsize_image(self, image: Image) -> Image:
-        """
-        Downsize an image using Pillow to 75% of its original size until its under 5k
-        """
-
-        scale_factor = 0.75
-
-        # Downsample to under 5K if applicable
-        while (True):
-            width, height = image.size
-            if width < 5000 and height < 5000:
-                break
-
-            new_width = int(width * scale_factor)
-            new_height = int(height * scale_factor)
-
-            # Use LANCZOS filter for high-quality downsampling
-            image = image.resize((new_width, new_height), Image.Resampling.LANCZOS)
-            
-        return image
-
     def open_image(self, img_index: int, image_locations: list) -> Image:
         if not image_locations:
             raise RuntimeError("No images found in the folder provided.")
+        
+        # Check that the image is under 50 MB to prevent out of memory issues
+        image_size_in_mb = os.path.getsize(image_locations[img_index]) / (1000 * 1000)
+        if image_size_in_mb > 50:
+            raise RuntimeError(f"image file {image_locations[img_index]} is required to be under 50 MB")
+            
 
         # Open the image using Pillow
         try:
             image = Image.open(image_locations[img_index])
-            image = self.downsize_image(image)
         except Exception as e:
             logger.error(f"Failed to read image file: {str(e)}")
             raise RuntimeError("Failed to read image file.")
@@ -56,12 +40,10 @@ class ImageFolder(BasePlugin):
         # Get a list of files in the folder
         images_in_folder = glob.glob(f"{directory_to_search}/**/*", recursive=True)
 
-        # print(images_in_folder)
-
-        img_index = random.randint(0, len(images_in_folder))
+        img_index = random.randrange(0, len(images_in_folder))
         image = self.open_image(img_index, images_in_folder)
 
-        ###
+        # pad the image to the screen dimensions when enabled
         if settings.get('padImage') == "true":
             dimensions = device_config.get_resolution()
             if device_config.get_config("orientation") == "vertical":
