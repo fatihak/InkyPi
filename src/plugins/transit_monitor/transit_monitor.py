df0ccc7b9e0c712df252112479c89e607a1be55c
JamesKohlsRepo <jameskohls@live.com>
2025-10-19 21:44:51 -0700

    changed registration

---
+++ src/plugins/transit_monitor/transit_monitor.py
@@ -0,0 +1,5 @@
+from base_plugin import BasePlugin
+
+class TransitMonitor(BasePlugin):
+    def generate_image(self, settings, device_config):
+        raise RuntimeError("stub")  # replace with real render
