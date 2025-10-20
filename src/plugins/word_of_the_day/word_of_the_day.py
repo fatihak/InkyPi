af47cd2862682460c946e720d66b679105678004
Rashgaron <dannirodriguez99@gmail.com>
2025-10-20 21:02:45 +0100

    Make API call

--- src/plugins/word_of_the_day/word_of_the_day.py
+++ src/plugins/word_of_the_day/word_of_the_day.py
@@ -12,15 +12,16 @@ import os
 
 logger = logging.getLogger(__name__)
 
+
 class WordOfTheDay(BasePlugin):
     def generate_settings_template(self):
         template_params = super().generate_settings_template()
-        template_params['api_key'] = {
+        template_params["api_key"] = {
             "required": True,
             "service": "OpenAI",
-            "expected_key": "OPEN_AI_SECRET"
+            "expected_key": "OPEN_AI_SECRET",
         }
-        template_params['style_settings'] = True
+        template_params["style_settings"] = True
         return template_params
 
     def generate_image(self, settings, device_config):
@@ -28,19 +29,17 @@ class WordOfTheDay(BasePlugin):
         if not api_key:
             raise RuntimeError("OPEN AI API Key not configured.")
 
-        title = settings.get("title")
+        text_lang = settings.get("wordLang")
+        if not text_lang:
+            raise RuntimeError("Text lang is required.")
 
-        text_model = settings.get('textModel')
+        text_model = settings.get("textModel")
         if not text_model:
             raise RuntimeError("Text Model is required.")
 
-        text_prompt = settings.get('textPrompt', '')
-        if not text_prompt.strip():
-            raise RuntimeError("Text Prompt is required.")
-
         try:
-            ai_client = OpenAI(api_key = api_key)
-            prompt_response = AIText.fetch_text_prompt(ai_client, text_model, text_prompt)
+            ai_client = OpenAI(api_key=api_key)
+            prompt_response = self.fetch_text_prompt(ai_client, text_model, text_lang)
         except Exception as e:
             logger.error(f"Failed to make Open AI request: {str(e)}")
             raise RuntimeError("Open AI request failure, please check logs.")
@@ -50,48 +49,37 @@ class WordOfTheDay(BasePlugin):
             dimensions = dimensions[::-1]
 
         image_template_params = {
-            "title": title,
             "content": prompt_response,
-            "plugin_settings": settings
+            "plugin_settings": settings,
         }
-        
-        image = self.render_image(dimensions, "ai_text.html", "ai_text.css", image_template_params)
+
+        image = self.render_image(
+            dimensions, "ai_text.html", "ai_text.css", image_template_params
+        )
 
         return image
-    
+
     @staticmethod
-    def fetch_text_prompt(ai_client, model, text_prompt):
-        logger.info(f"Getting random text prompt from input {text_prompt}, model: {model}")
+    def fetch_text_prompt(ai_client, model, text_lang):
+        logger.info(
+            f"Getting random text prompt from in {text_lang}, model: {model}"
+        )
 
         system_content = (
-            "You are a highly intelligent text generation assistant. Generate concise, "
-            "relevant, and accurate responses tailored to the user's input. The response "
-            "should be 70 words or less."
-            "IMPORTANT: Do not rephrase, reword, or provide an introduction. Respond directly "
-            "to the request without adding explanations or extra context "
-            "IMPORTANT: If the response naturally requires a newline for formatting, provide "
-            "the '\n' newline character explicitly for every new line. For regular sentences "
-            "or paragraphs do not provide the new line character."
-            f"For context, today is {datetime.today().strftime('%Y-%m-%d')}"
+            "You are an assistant that gives a word of the day in JSON format."
         )
-        user_content = text_prompt
+        user_content = f"Give me the word of the day in {text_lang} with fields: word, type, description, and example."
 
         # Make the API call
         response = ai_client.chat.completions.create(
             model=model,
             messages=[
-                {
-                    "role": "system",
-                    "content": system_content
-                },
-                {
-                    "role": "user",
-                    "content": user_content
-                }
+                {"role": "system", "content": system_content},
+                {"role": "user", "content": user_content},
             ],
-            temperature=1
+            temperature=1,
         )
 
-        prompt = response.choices[0].message.content.strip()
+        prompt = response.choices[0].message.content
         logger.info(f"Generated random text prompt: {prompt}")
         return prompt
