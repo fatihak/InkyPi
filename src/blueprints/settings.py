21065be654b2d5c75502b205c03056737ca10bcd
Aarón Riveiro <aaronriveiro@gmail.com>
2026-08-18 10:35:02 +0200

    feat: reorganize settings page into categories

--- src/blueprints/settings.py
+++ src/blueprints/settings.py
@@ -53,7 +53,6 @@ def save_settings():
             return jsonify({"error": "Plugin cycle interval must be less than 24 hours"}), 400
 
         settings = {
-            "name": form_data.get("deviceName"),
             "orientation": form_data.get("orientation"),
             "inverted_image": form_data.get("invertImage"),
             "log_system_stats": form_data.get("logSystemStats"),
